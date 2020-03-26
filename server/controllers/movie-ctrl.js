201100d1e877b81140285e906182374aaa72d345
Chris Tack <chris_tack@mac.com>
2020-03-26 16:16:10 -0700

    Create updateMovie function

---
+++ server/controllers/movie-ctrl.js
@@ -0,0 +1,73 @@
+const Movie = require('../models/movie-model');
+
+createMovie = (req, res) => {
+    const body = req.body
+
+    if (!body) {
+        return res.status(400).json({
+            success: false,
+            error: 'You must provide a movie!',
+        })
+    }
+
+    const movie = new Movie(body);
+
+    if(!movie) {
+        return res.status(400).json({ success: false, error: err });
+    }
+
+    movie
+        .save()
+        .then(() => {
+            return res.status(201).json({
+                success: true,
+                id: movie._id,
+                message: 'Movie created!',
+            })
+        })
+        .catch(error => {
+            return res.status(400).json({
+                error,
+                message: 'Movie not created!',
+            })
+        })
+}
+
+updateMovie = async (req, res) => {
+    const body = req.body;
+
+    if (!body) {
+        return res.status(400).json({
+            success: false,
+            error: 'Movie not found',
+        })
+    }
+
+    Movie.findOne({ _id: req.params.id }, (err, movie) => {
+        if (err) {
+            return res.status(404).json({
+                err,
+                message: 'Movie not found!',
+            })
+        }
+        movie.name = body.name
+        movie.time = body.time
+        movie.rating = body.rating
+        movie
+            .save()
+            .then(() => {
+                return res.status(200).json({
+                    success: true,
+                    id: movie._id,
+                    message: 'Movie Updated!',
+                })
+            })
+            .catch(error => {
+                return res.status(404).json({
+                    error,
+                    message: 'Movie not updated!',
+                })
+            })
+    })
+}
+
