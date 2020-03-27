b6f551d026848fafe9a7b7d822e7c1c5304d0a9e
Chris Tack <chris_tack@mac.com>
2020-03-27 13:55:19 -0700

    Create MoviesUpdate view and class

---
+++ client/src/pages/MoviesUpdate.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MoviesUpdate extends Component {
+    render() {
+        return (
+            <div>
+                <p>On this page, you'll see the form to update movies</p>
+            </div>
+        )
+    }
+}
+
+export default MoviesUpdate;
