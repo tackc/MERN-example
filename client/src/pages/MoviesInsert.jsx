22e970435faae2992efe9f4f4b34b5823a5ed316
Chris Tack <chris_tack@mac.com>
2020-03-27 13:53:23 -0700

    Create pages to display content

---
+++ client/src/pages/MoviesInsert.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MoviesInsert extends Component {
+    render() {
+        return (
+            <div>
+                <p>On this page, you'll see the form to add a movie!</p>
+            </div>
+        )
+    }
+}
+
+export default MoviesInsert;
