f44d5ef761b5687a2adf352d909f99c014e6a671
Chris Tack <chris_tack@mac.com>
2020-03-27 14:06:49 -0700

    Add state and componentDidMount

--- client/src/pages/MoviesList.jsx
+++ client/src/pages/MoviesList.jsx
@@ -6,7 +6,31 @@ import styled from 'styled-components';
 
 import 'react-table/react-table.css';
 
+const Wrapper = styled.div`
+    padding: 0 40px 40px 40px;
+`
+
 class MoviesList extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            movies: [],
+            columns: [],
+            isLoading: false,
+        }
+    }
+
+    componentDidMount = async () => {
+        this.setState({ isLoading: true });
+
+        await api.getAllMovies().then(movies => {
+            this.setState({
+                movies: movies.data.data,
+                isLoading: false,
+            })
+        })
+    }
+    
     render() {
         return(
             <div>
